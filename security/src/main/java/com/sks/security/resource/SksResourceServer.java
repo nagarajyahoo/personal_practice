254e1f6628d16b3b7633e57233b9cb1930da611b
nmr <nmr@123>
2019-03-17 16:06:37 +0000

    nmr: added security components

--- security/src/main/java/com/sks/security/resource/SksResourceServer.java
+++ security/src/main/java/com/sks/security/resource/SksResourceServer.java
@@ -1,8 +1,6 @@
 package com.sks.security.resource;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -23,20 +21,5 @@ public class SksResourceServer extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenServices(tokenServices);
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        //@formatter:off
-        http.authorizeRequests()
-                .antMatchers("/token").permitAll()
-                .antMatchers("/api/**").authenticated()
-                .and()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-            .csrf()
-                .disable();
-        //@formatter:on
     }
 }
