6c2952e7387490bda0c16fa2d6a72eb01a029c7f
nmr <nmr@123>
2019-04-01 19:33:13 +0100

    nmr: added html components

--- api/frontend/src/components/SksMain.js
+++ api/frontend/src/components/SksMain.js
@@ -2,39 +2,43 @@ import React from 'react';
 import Some from "./some/Some";
 import Hello from "./dummy/Hello";
 import {Route, Switch} from "react-router";
-import {Navbar, Nav, NavDropdown} from "react-bootstrap";
 import './SksMain.css';
 
 export default class SksMain extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-info static-top">
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
                     <div className="container">
-                        <a className="navbar-brand" href="#">
-                            <img src="favicon.ico" alt="" className={'rounded-circle'}/>
-                        </a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                                aria-label="Toggle navigation">
+                        <button
+                            className="navbar-toggler"
+                            data-toggle="collapse"
+                            data-target="#navbarCollapse">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarResponsive">
-                            <ul className="navbar-nav ml-auto">
+                        <div id="navbarCollapse" className="collapse navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
                                 <li className="nav-item active">
-                                    <a className="nav-link" href="#">Home
-                                        <span className="sr-only">(current)</span>
-                                    </a>
+                                    <a href="index.html" className="nav-link">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">About</a>
+                                    <a href="about.html" className="nav-link">About Us</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Services</a>
+                                    <a href="gallery.html" className="nav-link">Gallery</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Contact</a>
+                                    <a href="events.html" className="nav-link">Events</a>
                                 </li>
+                                <li className="nav-item">
+                                    <a href="contact.html" className="nav-link">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="sks-login">
+                            <ul>
+                                <li>Register</li>
+                                <li>Log In</li>
                             </ul>
                         </div>
                     </div>
