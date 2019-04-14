759d179a81eedfad2066f395391001db6075eb8b
nmr <nmr@123>
2019-04-14 18:00:13 +0100

    nmr: added about screen

--- api/frontend/src/components/aboutus/AboutUs.js
+++ api/frontend/src/components/aboutus/AboutUs.js
@@ -2,14 +2,21 @@ import React from 'react';
 import TitleSection from "../titlesec/TitleSection";
 import Row from "reactstrap/es/Row";
 import Col from "reactstrap/es/Col";
+import './About.css';
 
 export const aboutUs = () => {
     return (
-        <div>
+        <div className={'sks-about'}>
             <TitleSection title={'WANT TO KNOW MORE ABOUT US ?'}/>
             <Row>
                 <Col>
-                    <div>About Us</div>
+                    <div className={'sks-about-details'}>
+                        <p className="lead pb-4 pt-4">
+                            Scottish Karnataka Sangha is a socio-cultural organisation formed by the people of Karnataka
+                            (Indian) origin residing in various cities of Scotland, to promote the kannada - culture
+                            tradition and values.
+                        </p>
+                    </div>
                 </Col>
             </Row>
         </div>
