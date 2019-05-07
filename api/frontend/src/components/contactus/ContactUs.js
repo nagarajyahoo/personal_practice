cf4a0cbdb8fc53a6f01574ea202a5b8214df3db9
nmr <nmr@123>
2019-05-07 21:13:04 +0100

    nmr: fixed width for contact us

--- api/frontend/src/components/contactus/ContactUs.js
+++ api/frontend/src/components/contactus/ContactUs.js
@@ -19,71 +19,74 @@ export default class ContactUs extends React.Component {
         return (
             <div className="sks-contact-us">
                 <TitleSection title={'WE ALWAYS WELCOME YOUR QUERIES, CONTACT US'}/>
-                <Row>
-                    <Col sm={10} md={4} lg={4} className={'card mr-4 ml-4'}>
-                        <div className={'card-body'}>
-                            <h4>Get In Touch</h4>
-                            <p className="text-muted">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-                                rerum, laborum ab eos id optio!
-                            </p>
-                            <h5>Address</h5>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <h5>Email</h5>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <h5>Telephone</h5>
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-                    </Col>
-                    <Col sm={12} md={7} lg={7} className={'card p-4'}>
-                        <div className={'card-body'}>
-                            <h3>Please fill out this form</h3>
-                            <form>
-                                <div className="input-group input-group-lg mb-3">
-                                    <div className="input-group-prepend">
+                <div className="sks-contact-us-inner">
+                    <Row>
+                        <Col sm={10} md={4} lg={4} className={'card mr-4 ml-4'}>
+                            <div className={'card-body'}>
+                                <h4>Get In Touch</h4>
+                                <p className="text-muted">
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+                                    rerum, laborum ab eos id optio!
+                                </p>
+                                <h5>Address</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <h5>Email</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                                <h5>Telephone</h5>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </Col>
+                        <Col sm={12} md={7} lg={7} className={'card p-4'}>
+                            <div className={'card-body'}>
+                                <h3>Please fill out this form</h3>
+                                <form>
+                                    <div className="input-group input-group-lg mb-3">
+                                        <div className="input-group-prepend">
                                       <span className="input-group-text">
                                         <i className="fas fa-user"/>
                                       </span>
+                                        </div>
+                                        <input type="text"
+                                               className="form-control"
+                                               placeholder="Name"
+                                               onChange={() => this.handleChange()}/>
                                     </div>
-                                    <input type="text"
-                                           className="form-control"
-                                           placeholder="Name"
-                                           onChange={() => this.handleChange()}/>
-                                </div>
-                                <div className="input-group input-group-lg mb-3">
-                                    <div className="input-group-prepend">
+                                    <div className="input-group input-group-lg mb-3">
+                                        <div className="input-group-prepend">
                                       <span className="input-group-text">
                                         <i className="fas fa-envelope"/>
                                       </span>
+                                        </div>
+                                        <input type="email" className="form-control" placeholder="Email"
+                                               onChange={() => this.handleChange()}/>
                                     </div>
-                                    <input type="email" className="form-control" placeholder="Email"
-                                           onChange={() => this.handleChange()}/>
-                                </div>
-                                <div className="input-group input-group-lg mb-3">
-                                    <div className="input-group-prepend">
+                                    <div className="input-group input-group-lg mb-3">
+                                        <div className="input-group-prepend">
                                       <span className="input-group-text">
                                         <i className="fas fa-phone"/>
                                       </span>
+                                        </div>
+                                        <input type="phone" className="form-control" placeholder="Telephone"
+                                               onChange={() => this.handleChange()}/>
                                     </div>
-                                    <input type="phone" className="form-control" placeholder="Telephone"
-                                           onChange={() => this.handleChange()}/>
-                                </div>
-                                <div className="input-group input-group-lg mb-3">
-                                    <div className="input-group-prepend">
+                                    <div className="input-group input-group-lg mb-3">
+                                        <div className="input-group-prepend">
                               <span className="input-group-text">
                                 <i className="fas fa-pencil-alt"/>
                               </span>
+                                        </div>
+                                        <textarea
+                                            className="form-control"
+                                            placeholder="Message"
+                                            rows="3"/>
                                     </div>
-                                    <textarea
-                                        className="form-control"
-                                        placeholder="Message"
-                                        rows="3"/>
-                                </div>
-                                <Button color={'primary'} className={'btn-lg btn-block'} onClick={ () => false}>Submit</Button>
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
+                                    <Button color={'primary'} className={'btn-lg btn-block'}
+                                            onClick={() => false}>Submit</Button>
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         );
     }
